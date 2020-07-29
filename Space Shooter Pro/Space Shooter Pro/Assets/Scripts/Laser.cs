d36a836480148cb36f75ae777b5db49091d0f744
RahevinSlade <rahevinslade@gmail.com>
2020-07-28 18:06:30 -0700

    Added enemy and lasers

---
+++ Space Shooter Pro/Space Shooter Pro/Assets/Scripts/Laser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 8f;
+    //Speed variable of 8
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Translate laser up
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        //Check if postion on y is greater than 8, destroy object
+
+        if(transform.position.y >= 8f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Space Shooter Pro/Space Shooter Pro/Assets/Scripts/Player.cs
+++ Space Shooter Pro/Space Shooter Pro/Assets/Scripts/Player.cs
@@ -11,6 +11,12 @@ public class Player : MonoBehaviour
     //optional, value assigned
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.15f;
+
+    private float _canFire = -1f;
 
 
     // Start is called before the first frame update
@@ -23,25 +29,35 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+
+        //When I hit space key, spawn object
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time >= _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    void FireLaser()
+    {
+            _canFire = _fireRate + Time.time;
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime); //DeltaTime is real time movement (Side to side)
         //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime); // (Up and Down)
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);//cleaned up with local variable
+
         transform.Translate(direction * _speed * Time.deltaTime); // Optimized 
 
         //If player postion on the Y is greater than 0, then, y postion = 0
-        if(transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-            //run this code
-        } 
-        else if(transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
 
-        }
         //If player on the x > 11 then x pos = -11 else if player on x is less than -11 then x pos = 11
         if(transform.position.x >= 11.3f)
         {
@@ -52,6 +68,5 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(11.3f, transform.position.y, 0);
 
         }
-
     }
 }
